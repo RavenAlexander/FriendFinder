b8a107fd24617e23e79329300efe55598ca4db22
ralexander <theravenalexander@gmail.com>
2018-10-23 10:48:36 -0600

    my first commit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,49 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendData = require("../data/friends");
+//var githubData = require("../data/github");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+  // app.get("/api/github", function(req, res) {
+  //   res.json(githubData);
+  // });
+
+  // API POST Requests
+  // ---------------------------------------------------------------------------
+
+//   //app.post("/api/tables", function(req, res) {
+//     if (tableData.length < 5) {
+//       tableData.push(req.body);
+//       res.json(true);
+//     }
+//     else {
+//       waitListData.push(req.body);
+//       res.json(false);
+//     }
+//   });
+
+
+};
+
+
+
+
---
+++ app/server.js
@@ -0,0 +1,29 @@
+//code Your server.js file should require the basic npm packages we've used in class: express and path.
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
